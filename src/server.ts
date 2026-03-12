c3458e8ecf09b2454a7cac733e7e9b8ef07f9f53
Naominm <naomimbugua349@gmail.com>
2026-03-12 09:54:21 +0300

    Feat: Mount the access request controller at the server file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { type Express } from "express";
 import dotenv from "dotenv";
+import { createAccessRequest } from "./controllers/access.Request.js";
 
 dotenv.config();
 const app: Express = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use("/access-request", createAccessRequest);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`🚀 Server running on port ${port}`);
 });
