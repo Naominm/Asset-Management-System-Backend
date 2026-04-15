4fe1911c021ab5debb1ecbbc2f073e4d47de17ad
Naominm <naomimbugua349@gmail.com>
2026-04-15 14:51:39 +0300

    Fix: Fixed the error at email transport Service

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,3 +1,4 @@
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
@@ -8,7 +9,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+  tls: {
+    rejectUnauthorized: false,
+  },
+} as nodemailer.TransportOptions);
 
 interface AccessEmail {
   to: string;
